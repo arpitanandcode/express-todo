ee77928b664514616c7e07f436575fc8a1eb7abb
arpitanandtech <arpitanandtech@gmail.com>
2024-06-25 21:30:04 +0530

    feat: ejs, body-parser, routes

---
+++ database/db.js
@@ -0,0 +1,20 @@
+const todoList = [
+  {
+    id: 1,
+    title: 'Wake up'
+  },
+  {
+    id: 2,
+    title: 'Have Breakfast'
+  },
+  {
+    id: 3,
+    title: 'Go to office'
+  },
+  {
+    id: 4,
+    title: 'Fill water bottle'
+  },
+];
+
+export default todoList;
--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const express = require('express');
-const path = require('path');
-
+import express from 'express';
+import todoList from './database/db.js';
+import bodyParser from 'body-parser';
+import { v4 as uuidv4 } from 'uuid';
 const app = express();
 
-const PATH = path.join(__dirname, 'partials');
+app.set('view engine', 'ejs'); // Use to set the template engine
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.sendFile(PATH + "/index.html");
-})
+  const data = todoList;
+  res.render('index.ejs', { title: 'To do ', data: data });
+});
 
-app.get('/profile/', function (req, res) {
-  res.sendFile(PATH + "/about.html");
-})
+app.get('/add', function (req, res) {
+  res.render('add.ejs', { title: 'Add to do list' });
+});
+
+app.post('/add/mypost', function (req, res) {
+  // need the submitted value
+  // body-parser
+  const list = req.body.todo;
+  const obj = { id: uuidv4(), title: list };
+  todoList.push(obj);
+
+  res.redirect('/');
+});
 
 app.listen(8080, function () {
   console.log("App is working on 8080")
